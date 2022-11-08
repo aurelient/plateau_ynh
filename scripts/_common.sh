59203c9c1ca711aaa9676547371441a441e790a8
Guillaume Bouzige <guillaume.bouzige+github@ddataa.org>
2022-11-08 09:34:05 +0100

    first try

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,6 @@
 
 # dependencies used by the app (must be on a single line)
 #pkg_dependencies="libxtst6 libxss1 gconf-gsettings-backend libpangocairo-1.0-0 libnss3 libnss3-tools"
-pkg_dependencies=""
-#nodejs_version="16.13"
 nodejs_version="16.14.2"
 
 #=================================================
